ebcf259434c26897f8a7e9bcdc8c777e644b4a2b
Aarya <amathrej@asu.edu>
2022-06-15 05:03:30 -0700

    Add variables for left and right split arrays

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -4,13 +4,15 @@ def merge_sort(unsorted_arr)
   array_length = unsorted_arr.length
   return unless array_length > 1
 
-  merge_sort(unsorted_arr[0, (array_length / 2)])
-  merge_sort(unsorted_arr[(array_length / 2), array_length])
-  merge_sort_helper(unsorted_arr[0, (array_length / 2)], unsorted_arr[(array_length / 2), array_length])
+  array_left_half = unsorted_arr[0, (array_length / 2)]
+  array_right_half = unsorted_arr[(array_length / 2), array_length]
+  merge_sort(array_left_half)
+  merge_sort(array_right_half)
+  merge_sort_helper(array_left_half, array_right_half)
 end
 
 # merge_sort_helper takes in 2 arrays and sorts them according to merge sort
-def merge_sort_helper(arr1, arr2)
+def merge_sort_helper(arr, arr1, arr2)
   sorted_array = []
   arr1_index = 0
   arr2_index = 0
@@ -35,11 +37,13 @@ def merge_sort_helper(arr1, arr2)
     arr2_index += 1
   end
 
-  sorted_array
+  arr = sorted_array
 end
 
 arr1 = [6, 2, 8, 3]
 arr2 = [1, 4, 5]
+
+#p merge_sort_helper([2, 5, 8], [1, 3, 6, 7])
 
 puts "Before sorting: #{arr1 + arr2}"
 puts "After sorting: #{merge_sort(arr1 + arr2)}"
