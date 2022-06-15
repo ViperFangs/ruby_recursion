3708044712b1ec15dcb093cdf5ecefab7e9cd6c4
Aarya <amathrej@asu.edu>
2022-06-15 02:47:22 -0700

    Write solution for merge_sort & merge_sort_helper

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -6,5 +6,35 @@ def merge_sort(unsorted_arr)
 
   merge_sort(unsorted_arr[0...slice_number])
   merge_sort(unsorted_arr[slice_number..array_length])
-  #sort the array
+  merge_sort_helper(unsorted_arr[0...slice_number], unsorted_arr[slice_number..array_length])
 end
+
+def merge_sort_helper(arr1, arr2)
+  sorted_array = []
+  arr1_index = 0 
+  arr2_index = 0
+
+  while arr1_index < arr1.length && arr2_index < arr2.length
+    if arr1[arr1_index] < arr2[arr2_index]
+      sorted_array.push(arr1[arr1_index])
+      arr1_index += 1
+    else
+      sorted_array.push(arr2[arr2_index])
+      arr2_index += 1
+    end
+  end
+
+  if arr1_index == arr1.length + 1
+    sorted_array += arr2[arr2_index..arr2.length]
+  else
+    sorted_array += arr1[arr1_index..arr1.length]
+  end
+
+  sorted_array
+end
+
+arr1 = [2,3,8,9]
+arr2 = [1,4,5,7]
+
+p arr1 + arr2
+p merge_sort(arr1 + arr2)
